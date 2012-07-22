937224d821b137a7b6d4affec56bf1ad81537f6b
jas <jasperbae@yahoo.com>
2012-07-22 15:05:08 -0400

    Name change
    Changed from Jasper to Philip

--- Example/src/com/ex/examp/HelloWorld.java
+++ Example/src/com/ex/examp/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		System.out.println("Hello...");
-		System.out.print("\nMy name is Jasper.");
+		System.out.print("\nMy name is Philip");
 	}
 }
